c0d74f73f6204166e6e89f1e5aa26a17109215a5
Rakesh Arunachalam <rakesh@thoughtbot.com>
2019-11-22 14:46:33 +0000

    short way to initialize immer produce

--- app/reducers/language.js
+++ app/reducers/language.js
@@ -1,10 +1,10 @@
 import produce from 'immer';
 import languages from '../languages';
 
-const LANGUAGE_STATE = {
-  languages,
-  defaultLanguage: 'en',
-};
+// const LANGUAGE_STATE = {
+//   languages,
+//   defaultLanguage: 'en',
+// };
 
 //Normal redux reducer
 // export default (state = LANGUAGE_STATE, action) => {
@@ -14,18 +14,39 @@ const LANGUAGE_STATE = {
 //   }
 // };
 
-export default produce((draft, action) => {
-  switch (action.type) {
-    case 'CHANGE_LANGUAGE':
-      if (draft.defaultLanguage === 'ar') {
-        draft.defaultLanguage = 'en';
-      } else if (draft.defaultLanguage === 'en') {
-        draft.defaultLanguage = 'fr';
-      } else if (draft.defaultLanguage === 'fr') {
-        draft.defaultLanguage = 'ar';
-      }
-      return;
-    default:
-      return LANGUAGE_STATE;
-  }
-});
+//Immer reducer with default state outside the produce function
+// export default produce((draft, action) => {
+//   switch (action.type) {
+//     case 'CHANGE_LANGUAGE':
+//       if (draft.defaultLanguage === 'ar') {
+//         draft.defaultLanguage = 'en';
+//       } else if (draft.defaultLanguage === 'en') {
+//         draft.defaultLanguage = 'fr';
+//       } else if (draft.defaultLanguage === 'fr') {
+//         draft.defaultLanguage = 'ar';
+//       }
+//       return;
+//     default:
+//       return LANGUAGE_STATE;
+//   }
+// });
+
+export default produce(
+  (draft, action) => {
+    switch (action.type) {
+      case 'CHANGE_LANGUAGE':
+        if (draft.defaultLanguage === 'ar') {
+          draft.defaultLanguage = 'en';
+        } else if (draft.defaultLanguage === 'en') {
+          draft.defaultLanguage = 'fr';
+        } else if (draft.defaultLanguage === 'fr') {
+          draft.defaultLanguage = 'ar';
+        }
+        return;
+    }
+  },
+  {
+    languages,
+    defaultLanguage: 'en',
+  },
+);
