df9d683b953da566c85523f35328ad8a7bd7df41
Rakesh Arunachalam <rakesh@thoughtbot.com>
2019-11-22 14:40:43 +0000

    Use reselect instead of computations every time

--- app/selectors/index.js
+++ app/selectors/index.js
@@ -4,3 +4,11 @@ export const currentLanguage = createSelector(
   state => state.language,
   ({languages, defaultLanguage}) => languages[defaultLanguage],
 );
+
+// Code below is an example to show why we use reselect
+
+// export const currentLanguage = state => {
+//   const {languages, defaultLanguage} = state.language;
+//   console.log('currentLanguage selector', defaultLanguage);
+//   return languages[defaultLanguage];
+// };
